24be45ab0ee646f0adcebe997494ced53d3b0ea9
Thiago Marim <thiago.marim2005@gmail.com>
2025-07-01 19:32:18 -0300

    feat: add loop structure exercises

---
+++ src/com/thiagomarim/maratonajava/introducao/exercicios/EstruturaRepeticao.java
@@ -0,0 +1,10 @@
+package com.thiagomarim.maratonajava.introducao.exercicios;
+
+public class EstruturaRepeticao {
+    public static void main(String[] args) {
+        // Imprima todos os números pares de 0 até 1000000
+        for (int i = 0; i <= 1000000; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
