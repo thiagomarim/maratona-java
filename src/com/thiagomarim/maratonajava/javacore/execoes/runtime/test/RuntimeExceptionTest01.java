a5b88b94eeff6dedae0b6deac1d6032428fcf0bc
Thiago Marim <thiago.marim2005@gmail.com>
2025-07-30 15:43:43 -0300

    feat: Lesson 96 - Exceptions part 2: RuntimeException

---
+++ src/com/thiagomarim/maratonajava/javacore/execoes/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,12 @@
+package com.thiagomarim.maratonajava.javacore.execoes.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        // CHECKED / UNCHECKED
+
+//        Object object = null;
+//        System.out.println(object.toString());
+        int nums[] = {1, 2};
+        System.out.println(nums[2]);
+    }
+}
