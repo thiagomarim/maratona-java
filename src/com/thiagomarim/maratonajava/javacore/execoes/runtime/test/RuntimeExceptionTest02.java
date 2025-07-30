7b44b20977a2dd89f6db6e878546135bf20576a8
Thiago Marim <thiago.marim2005@gmail.com>
2025-07-30 16:22:30 -0300

    feat: Lesson 98 - Exceptions part 4: Throwing unchecked exceptions

---
+++ src/com/thiagomarim/maratonajava/javacore/execoes/runtime/test/RuntimeExceptionTest02.java
@@ -0,0 +1,27 @@
+package com.thiagomarim.maratonajava.javacore.execoes.runtime.test;
+
+public class RuntimeExceptionTest02 {
+    public static void main(String[] args) {
+        try {
+            divisao(1, 0);
+        } catch (ArithmeticException e ){
+            e.printStackTrace();
+        }
+
+        System.out.println("Código finalizado!");
+    }
+
+    /**
+     *
+     * @param a
+     * @param b nao pode ser 0
+     * @return
+     * @throws IllegalArgumentException caso B seja 0
+     */
+    public static int divisao(int a, int b) {
+        if (b != 0) {
+            throw new IllegalArgumentException("Argumento ilegal, nao pode ser 0!");
+        }
+        return a / b;
+    }
+}
