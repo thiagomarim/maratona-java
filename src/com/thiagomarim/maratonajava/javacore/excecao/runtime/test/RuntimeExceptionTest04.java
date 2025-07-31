9839d9a6a431c1f49761c176ecff76ddad7d7e98
Thiago Marim <thiago.marim2005@gmail.com>
2025-07-31 19:59:19 -0300

    feat: Lesson 102 - Exceptions part 8: Multi-catch in a single line

--- src/com/thiagomarim/maratonajava/javacore/excecao/runtime/test/RuntimeExceptionTest04.java
+++ src/com/thiagomarim/maratonajava/javacore/excecao/runtime/test/RuntimeExceptionTest04.java
@@ -7,23 +7,15 @@ public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         try {
             throw new ArrayIndexOutOfBoundsException();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Dentro de ArrayIndexOutOfBoundsException");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Dentro de IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Dentro de IllegalArgumentException");
-        } catch (ArithmeticException e) {
-            System.out.println("Dentro de ArithmeticException");
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
+            System.out.println("Dentro de ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException");
         } catch (RuntimeException e) {
             System.out.println("Dentro de RuntimeException");
         }
 
         try {
             talvezLanceUmaException();
-        } catch (SQLException e ){
-            e.printStackTrace();
-        } catch (FileNotFoundException e ){
+        } catch (SQLException | FileNotFoundException e) {
             e.printStackTrace();
         }
     }
