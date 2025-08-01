946999c460a414b62f375a6ecdc9958b1a1e8589
Thiago Marim <thiago.marim2005@gmail.com>
2025-08-01 15:27:46 -0300

    feat: Lesson 110 - Utility classes: Strings part 3 (performance)

---
+++ src/com/thiagomarim/maratonajava/javacore/string/test/StringPerformanceTest01.java
@@ -0,0 +1,41 @@
+package com.thiagomarim.maratonajava.javacore.string.test;
+
+public class StringPerformanceTest01 {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concatString(30_000);
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuilder(100_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String Builder " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuffer(100_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String Buffer " + (fim - inicio) + "ms");
+    }
+
+    private static void concatString(int tamanho) {
+        String texto = " ";
+        for (int i = 0; i < tamanho; i++) {
+            texto += i;
+        }
+    }
+
+    private static void concatStringBuilder(int tamanho) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < tamanho; i++) {
+            stringBuilder.append(i);
+        }
+    }
+
+    private static void concatStringBuffer(int tamanho) {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < tamanho; i++) {
+            stringBuffer.append(i);
+        }
+    }
+}
