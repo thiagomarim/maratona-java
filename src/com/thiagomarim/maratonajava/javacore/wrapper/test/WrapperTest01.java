1a7fa4fc557f9b28b1a19d186671cca292632658
Thiago Marim <thiago.marim2005@gmail.com>
2025-08-01 14:13:13 -0300

    feat: Lesson 106 - Utility classes: Wrappers part 1

---
+++ src/com/thiagomarim/maratonajava/javacore/wrapper/test/WrapperTest01.java
@@ -0,0 +1,23 @@
+package com.thiagomarim.maratonajava.javacore.wrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'T';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'T';
+        Boolean booleanW = false;
+    }
+}
