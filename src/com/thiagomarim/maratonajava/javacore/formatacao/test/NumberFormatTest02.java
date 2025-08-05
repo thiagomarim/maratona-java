debf1a8f211e5019b07be60b5de531d34f0aaf25
Thiago Marim <thiago.marim2005@gmail.com>
2025-08-05 19:24:08 -0300

    feat: lesson 117 - utility classes: currency internationalization with locale

--- src/com/thiagomarim/maratonajava/javacore/formatacao/test/NumberFormatTest02.java
+++ src/com/thiagomarim/maratonajava/javacore/formatacao/test/NumberFormatTest02.java
@@ -4,20 +4,20 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
     public static void main(String[] args) {
-        Locale localeBR = new Locale("pt", "BR");
+        Locale localeEN = new Locale("en", "US");
         Locale localeJP = Locale.JAPAN;
         Locale localeIT = Locale.ITALY;
 
         NumberFormat[] nfa = new NumberFormat[4];
 
-        nfa[0] = NumberFormat.getInstance();
-        nfa[1] = NumberFormat.getInstance(localeBR);
-        nfa[2] = NumberFormat.getInstance(localeJP);
-        nfa[3] = NumberFormat.getInstance(localeIT);
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeEN);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeIT);
 
-        NumberFormat nf2 = NumberFormat.getInstance();
+        NumberFormat nf2 = NumberFormat.getCurrencyInstance();
 
         double valor = 100_000_000.2130;
 
@@ -27,7 +27,7 @@ public class NumberFormatTest01 {
             System.out.println(nf.format(valor));
         }
 
-        String valorString = "1.000,00";
+        String valorString = "$1,000.00";
 
         try {
             System.out.println(nfa[1].parse(valorString));
