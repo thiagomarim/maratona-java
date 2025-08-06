11f39aeccef7cdcb71b726ec5cfe4734de2211c3
Thiago Marim <thiago.marim2005@gmail.com>
2025-08-06 17:02:31 -0300

    feat: lesson 125 - utility classes: chronounit

---
+++ src/com/thiagomarim/maratonajava/javacore/datas/test/ChronoUnitTest01.java
@@ -0,0 +1,16 @@
+package com.thiagomarim.maratonajava.javacore.datas.test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDateTime aniversario = LocalDateTime.of(2005, Month.JUNE, 22, 12, 0, 0);
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(ChronoUnit.DAYS.between(aniversario, now));
+        System.out.println(ChronoUnit.WEEKS.between(aniversario, now));
+        System.out.println(ChronoUnit.MONTHS.between(aniversario, now));
+        System.out.println(ChronoUnit.YEARS.between(aniversario, now));
+    }
+}
