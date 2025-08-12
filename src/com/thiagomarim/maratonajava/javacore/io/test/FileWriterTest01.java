fad7260b8f7e27ccbe96ab66e317cc2c53dcf81b
Thiago Marim <thiago.marim2005@gmail.com>
2025-08-12 19:41:56 -0300

    feat: lesson 140 - utility classes: io part 3 - filereader

--- src/com/thiagomarim/maratonajava/javacore/io/test/FileWriterTest01.java
+++ src/com/thiagomarim/maratonajava/javacore/io/test/FileWriterTest01.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class FileWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
-        try (FileWriter fw = new FileWriter(file)) {
+        try (FileWriter fw = new FileWriter(file, true)) {
             fw.write("O curso do Dev dojo é o melhor do Brasillllllllllllllllll\nContinuando em outra linha rs\n");
             fw.flush();
         } catch (IOException ex) {
